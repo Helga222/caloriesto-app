83d5ceaf7006f0aaf3c173fbe48a71b4e7f979b6
Helga222 <barykina.o@yandex.ru>
2023-05-19 18:27:48 +0300

    fixed

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -55,7 +55,7 @@ export const MainPage = (props: { meals: Meal[], name:string, calorieGoal: numbe
   return (
     <div className={styles.content}>
       <div className={styles.menu}>
-        <div>
+        <div className={styles.menu__item}>
           <button
             className={`${styles.menu__button} ${styles.menu__button__user} ${styles.menu__button__thin}`}
           >
@@ -71,8 +71,7 @@ export const MainPage = (props: { meals: Meal[], name:string, calorieGoal: numbe
           >
             {props.calorieGoal ? `Норма: ${props.calorieGoal} кКал` : 'Рассчитайте норму калорий в калькуляторе'}
           </button>
-        </div>
-        <div>
+
           <button onClick={()=>navigate(`/addmeal/${id}`)}
             className={`${styles.menu__button} ${styles.menu__button__add} ${styles.menu__button__thin}`}
           >
@@ -90,14 +89,14 @@ export const MainPage = (props: { meals: Meal[], name:string, calorieGoal: numbe
             Калькулятор калорий
           </button>
 
-        </div>
-        <div>
+
         <button onClick={props.onLogout}
             className={`${styles.menu__button} ${styles.menu__button__exit} ${styles.menu__button__thin}`}
           >
             Выход
           </button>
-        </div>
+          </div>
+          <div className={styles.menu__item}></div>
       </div>
       <div className={styles.foodList}>
        <MealTable deletable={0} products={props.meals[0].products} index={0} onEditMeal={props.onEditMeal} type={props.meals[0].type}></MealTable>
